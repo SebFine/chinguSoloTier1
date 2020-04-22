4d8693399186ff6d02bac714b76e27783a4db893
SebFine <62475734+SebFine@users.noreply.github.com>
2020-04-22 08:57:38 +0200

    update

---
+++ parallax.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    var parallax = document.getElementsByClassName('parallax');
+    var xScrollPosition;
+    var yScrollPosition;
+
+    for (let i = 0; i < parallax.length; i++) {
+        let element = parallax[i];
+        return element;
+    }
+
+    function setTranslate(xPos, yPos, el) {
+        el.style.transform = "translate3d(" + xPos + ", " + yPos + "px, 0)";
+    }
+
+    function scrollLoop() {
+      xScrollPosition = window.scrollX;
+      yScrollPosition = window.scrollY;
+
+      setTranslate(0, yScrollPosition * -0.2, element);
+
+  }
+
+    window.addEventListener("scroll", scrollLoop, false);
+
+});
