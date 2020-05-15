2311b87cf361ba6e83e19fb604c3f394ebf254ec
62475734 <62475734+SebFine@users.noreply.github.com>
2020-05-15 10:14:23 +0200

    update of parallax, added functionality, improvement of design

--- parallax2.js
+++ parallax2.js
@@ -1,11 +1,18 @@
-    let scrolled = 0;
-    window.addEventListener("scroll", () => {
-      scrolled = window.pageYOffset;
-      const parallax = document.querySelector(".banner");
-      let coords = '0% '+ (- (scrolled *0.38) + 'px');
-      parallax.style.backgroundPosition = coords;
-      console.log(coords);
-      console.log(scrolled)
-    });
 
-    // Just the for Loop and it´s done.
+
+window.addEventListener("scroll", () => {
+  scrolled = window.pageYOffset;
+  const parallax = document.querySelectorAll(".parallax");
+  for (const i of parallax) {
+    let coords = '0% ' + (-(scrolled * 0.3) + 'px');
+    i.style.backgroundPosition = coords;
+    console.log(coords);
+    console.log(scrolled)
+  }
+
+});
+
+// Just the for Loop and it´s done.
+
+/* window.removeEventListener('click', sameFunctionAsBefore) */
+
